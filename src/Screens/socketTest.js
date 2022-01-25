7b9ce35d79103722a6a694100b66f17d05378fa5
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-25 15:00:48 +0900

    example updated

--- src/Screens/socketTest.js
+++ src/Screens/socketTest.js
@@ -1,32 +1,58 @@
 
 import {Alert, Button, Component, Image, TouchableOpacity, StyleSheet, Text} from 'react-native';
 import React, {useState} from 'react';
-import io from 'socket.io-client';
+import {io} from 'socket.io-client';
 
 
 
 const SocketTest = () => {
-
-	this.socket = io("http://192.249.18.141:80");
-	this.socket.on("message", msg => {
-		console.log("got msg: ", msg);
-	});
 	let count = 0;
-
+	let socket = null;
 
 	const onPress = () => {
+		if(socket) {
+			console.log("send message!");
+			socket.emit("message", "test message from clent" + count.toString());
+			count = count + 1;
+		} else {
+			console.log("no socket!");
+		}
+	}
+
+	const connectSocket = () => {
+		socket = io("http://192.249.18.141:80");
 		socket.on('connect', () => {
 			console.log("connected!");
 		})
-		console.log("send message!: ", socket);
-		socket.emit("message", "test message from clent" + count.toString());
-		count = count + 1;
+		socket.on("message", msg => {
+			console.log("got msg: ", msg);
+		});
+	}
+
+	const disconnectSocket = () => {
+		if(socket) {
+			console.log("disconnected!");
+			socket.disconnect();
+			socket.off();
+			socket = null;
+		} else {
+			console.log("no socket!");
+		}
 	}
 
 	return (
-		<TouchableOpacity onPress={onPress}>
-			<Text>Send Message!</Text>
-		</TouchableOpacity>
+		<>
+			<TouchableOpacity onPress={onPress}>
+				<Text>Send Message!</Text>
+			</TouchableOpacity>
+			<TouchableOpacity onPress={connectSocket}>
+				<Text>Connect Socket</Text>
+			</TouchableOpacity>
+			<TouchableOpacity onPress={disconnectSocket}>
+				<Text>Disconnect Socket</Text>
+			</TouchableOpacity>
+		</>
+
 	)
 
 }
